01aeac2bf3c8a79d9967705196a9117cd70d5d74
Renan Osorio <renanosoriogd@gmail.com>
2023-07-04 19:13:09 -0300

    criado repo de premio

---
+++ Sistema/WebAPI/TcheDaSorte/TS.Data/Interfaces/IPremioRepository.cs
@@ -0,0 +1,10 @@
+﻿using TS.Model.Models;
+using TS.Models.Models;
+
+namespace TS.Data.Interfaces
+{
+    public interface IPremioRepository : IRepository<Premio>
+    {
+        Task<Premio> ObterPorCodigo(string codigo);
+    }
+}
---
+++ Sistema/WebAPI/TcheDaSorte/TS.Data/Repository/PremioRepository.cs
@@ -0,0 +1,20 @@
+﻿using TS.Data.Context;
+using TS.Data.Interfaces;
+using TS.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using TS.Model.Models;
+
+namespace TS.Data.Repository
+{
+    public class PremioRepository : Repository<Premio>, IPremioRepository
+    {
+        public PremioRepository(TSContext context) : base(context) { }
+
+        public async Task<Premio> ObterPorCodigo(string codigo)
+        {
+            return await _context.Premio
+                .Where(obj => obj.Codigo == codigo)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
