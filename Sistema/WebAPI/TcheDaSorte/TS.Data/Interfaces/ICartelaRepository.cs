ce37081aa3ef1f9d3a3a999eff710fce2ad889ea
Renan Osorio <renanosoriogd@gmail.com>
2023-07-04 19:17:02 -0300

    criado repo de cartela

---
+++ Sistema/WebAPI/TcheDaSorte/TS.Data/Interfaces/ICartelaRepository.cs
@@ -0,0 +1,10 @@
+﻿using TS.Model.Models;
+using TS.Models.Models;
+
+namespace TS.Data.Interfaces
+{
+    public interface ICartelaRepository : IRepository<Cartela>
+    {
+        Task<Cartela> ObterPorPremioId(int idPremio);
+    }
+}
---
+++ Sistema/WebAPI/TcheDaSorte/TS.Data/Repository/CartelaRepository.cs
@@ -0,0 +1,20 @@
+﻿using TS.Data.Context;
+using TS.Data.Interfaces;
+using TS.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using TS.Model.Models;
+
+namespace TS.Data.Repository
+{
+    public class CartelaRepository : Repository<Cartela>, ICartelaRepository
+    {
+        public CartelaRepository(TSContext context) : base(context) { }
+
+        public async Task<Cartela> ObterPorPremioId(int idPremio)
+        {
+            return await _context.Cartela
+                .Where(obj => obj.PremioId == idPremio)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
