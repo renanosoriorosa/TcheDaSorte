704eaa5cc5d36a1f36da890a1f73e36dc4a1e86c
Renan Osorio <renanosoriogd@gmail.com>
2023-07-04 19:58:45 -0300

    Ajustes injeção de dependencia

--- Sistema/WebAPI/TcheDaSorte/TcheDaSorte/Configuration/DependencyInjectionConfig.cs
+++ Sistema/WebAPI/TcheDaSorte/TcheDaSorte/Configuration/DependencyInjectionConfig.cs
@@ -2,12 +2,13 @@
 //using TS.API.Interfaces;
 //using TS.Core.Interfaces;
 //using TS.Core.Notificacoes;
-//using TS.Core.Services;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using TS.API.Extensions;
+using TS.API.Interfaces;
 using TS.Data.Context;
-//using TS.Data.Interfaces;
-//using TS.Data.Repository;
-//using Microsoft.Extensions.Options;
-//using Swashbuckle.AspNetCore.SwaggerGen;
+using TS.Data.Interfaces;
+using TS.Data.Repository;
 
 namespace TS.API.Configuration
 {
@@ -17,13 +18,12 @@ namespace TS.API.Configuration
         {
             services.AddScoped<TSContext>();
 
-            //services.AddScoped<INotificador, Notificador>();
-            //services.AddScoped<IProdutoService, ProdutoService>();
-            //services.AddScoped<IProdutoRepository, ProdutoRepository>();
-            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            //services.AddScoped<IUser, AspNetUser>();
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<ICartelaRepository, CartelaRepository>();
+            services.AddScoped<IPremioRepository, PremioRepository>();
+            services.AddScoped<IUser, AspNetUser>();
 
-            //services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
             return services;
         }   
