cb763b76583e7eb71a88f2a75cf5238ff61ad6ef
Renan Osorio <renanosoriogd@gmail.com>
2023-07-06 19:23:56 -0300

    nova migração

--- Sistema/WebAPI/TcheDaSorte/TS.Data/Migrations/20230706222325_AjusteIdIdentity.Designer.cs
+++ Sistema/WebAPI/TcheDaSorte/TS.Data/Migrations/20230706222325_AjusteIdIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TS.Data.Context;
 
@@ -11,9 +12,10 @@ using TS.Data.Context;
 namespace TS.Data.Migrations
 {
     [DbContext(typeof(TSContext))]
-    partial class TSContextModelSnapshot : ModelSnapshot
+    [Migration("20230706222325_AjusteIdIdentity")]
+    partial class AjusteIdIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Sistema/WebAPI/TcheDaSorte/TS.Data/Migrations/20230706222325_AjusteIdIdentity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TS.Data.Migrations
+{
+    public partial class AjusteIdIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IndentityId",
+                table: "Usuario",
+                type: "varchar(200)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IndentityId",
+                table: "Usuario",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldNullable: true);
+        }
+    }
+}
